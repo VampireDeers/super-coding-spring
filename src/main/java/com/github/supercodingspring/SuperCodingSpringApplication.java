054a0735b01003f70e0e3967f4078c36307e8db0
VampireDeers <likemin5517@naver.com>
2023-07-30 00:49:26 +0900

    feat: Spring project init

---
+++ src/main/java/com/github/supercodingspring/SuperCodingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.github.supercodingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperCodingSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperCodingSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/supercodingspring/SuperCodingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.supercodingspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperCodingSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
