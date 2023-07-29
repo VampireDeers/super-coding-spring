4ac42a24c3271aa3472cfd15ca4e488d7a73b86f
VampireDeers <likemin5517@naver.com>
2023-07-30 01:52:46 +0900

    feat: chapter_96 API 요구사항 추가 및 table 사진 추가

--- src/main/java/com/github/supercodingspring/SuperCodingSpringApplication.java
+++ src/main/java/com/github/supercodingspring/SuperCodingSpringApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SuperCodingSpringApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SuperCodingSpringApplication.class, args);
     }
