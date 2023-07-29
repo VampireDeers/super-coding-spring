083dfd7a036787d4c814a54dfe596db4cc7f4c4c
VampireDeers <likemin5517@naver.com>
2023-07-30 01:10:38 +0900

    feat: chapter_95 SQL 파일 추가

---
+++ sql/item_ddl_dml.sql
@@ -0,0 +1,8 @@
+INSERT INTO item(name, type, price, cpu, capacity)
+VALUES
+    ('Apple iPhone 12 Pro Max', 'Smartphone', 1490000, 'A14 Bionic', '512GB'),
+    ('Samsung Galaxy S21 Ultra', 'Smartphone', 1690000, 'Exynos 2100', '256GB'),
+    ('Google Pixel 6 Pro', 'Smartphone', 1290000, 'Google Tensor', '128GB'),
+    ('Dell XPS 15', 'Laptop', 2290000, 'Intel Core i9', '1TB SSD'),
+    ('Sony Alpha 7 III', 'Mirrorless Camera', 2590000, 'BIONZ X', 'No internal storage'),
+    ('Microsoft Xbox Series X', 'Gaming Console', 499000, 'Custom AMD Zen 2', '1TB SSD');
