7031c4b19d6bdc7ead8b171dd074ac18ab9bfa76
VampireDeers <likemin5517@naver.com>
2023-08-06 21:53:52 +0900

    feat: 실습-4 Spring Cache 설정 추가

--- src/main/java/com/github/supercodingspring/SuperCodingSpringApplication.java
+++ src/main/java/com/github/supercodingspring/SuperCodingSpringApplication.java
@@ -2,8 +2,10 @@ package com.github.supercodingspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SuperCodingSpringApplication {
     public static void main(String[] args) {
         SpringApplication.run(SuperCodingSpringApplication.class, args);
