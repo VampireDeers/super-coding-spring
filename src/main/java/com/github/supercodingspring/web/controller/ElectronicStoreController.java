23ecc29cfdc2c8a93238742aa60e3f378b4dc349
VampireDeers <likemin5517@naver.com>
2023-08-06 11:57:51 +0900

    chore: 각 메소드 마다 로깅 되어 있음.

--- src/main/java/com/github/supercodingspring/web/controller/ElectronicStoreController.java
+++ src/main/java/com/github/supercodingspring/web/controller/ElectronicStoreController.java
@@ -23,98 +23,61 @@ public class ElectronicStoreController {
 
     private final ElectronicStoreItemService electronicStoreItemService;
 
-    @ApiOperation("모든 Items을 검색")
-    @GetMapping("/items")
-    public List<Item> findAllItem(){
-        log.info("GET /items 요청이 들어왔습니다.");
-        List<Item> items = electronicStoreItemService.findAllItem();
-        log.info("GET /items 응답: " + items);
-        return items;
-    }
-
-    @ApiOperation("단일 Item 등록")
-    @PostMapping("/items")
-    public String registerItem(@RequestBody ItemBody itemBody){
-        Integer itemId = electronicStoreItemService.savaItem(itemBody);
-        return "ID: " +  itemId;
-    }
-
-    @ApiOperation("단일 Item id로 검색")
-    @GetMapping("/items/{id}")
-    public Item findItemByPathId(
-            @ApiParam(name = "id", value = "item ID", example = "1") @PathVariable String id){
-        return electronicStoreItemService.findItemById(id);
-    }
-
     @ApiOperation("단일 Item id로 검색 (쿼리문)")
     @GetMapping("/items-query")
     public Item findItemByQueryId(
             @ApiParam(name = "id", value = "item ID", example = "1")
             @RequestParam("id") String id){
-        return electronicStoreItemService.findItemById(id);
+        log.info("GET /items-query 요청이 들어왔습니다.");
+        Item item = electronicStoreItemService.findItemById(id);
+        log.info("GET /items-query 응답: " + item);
+        return item;
     }
 
-    @ApiOperation("여러 Item ids로 검색 (쿼리문)")
-    @GetMapping("/items-queries")
-    public List<Item> findItemByQueryIds(@ApiParam(name = "ids", value = "item IDs", example = "[1,2,3]") @RequestParam("id") List<String> ids){
-        log.info("/items-queries 요청 ids: " + ids);
-        List<Item> items = electronicStoreItemService.findItemsByIds(ids);
-        log.info("/items-queries 응답: " + items);
-        return items;
-    }
-
-    @ApiOperation("단일 Item id로 삭제")
-    @DeleteMapping("/items/{id}")
-    public String deleteItemByPathId(@ApiParam(name = "id", value = "item ID", example = "1")  @PathVariable String id){
-        electronicStoreItemService.deleteItem(id);
-        return "Object with id =" + id + "has been deleted";
-    }
-
-    @ApiOperation("단일 Item id 수정")
-    @PutMapping("/items/{id}")
-    public Item updateItem(@PathVariable String id, @RequestBody ItemBody itemBody){
-        return electronicStoreItemService.updateItem(id, itemBody);
-    }
-
-    @ApiOperation("단일 Item 구매 ")
-    @PostMapping("/items/buy")
-    public String buyItem(@RequestBody BuyOrder buyOrder){
-        Integer orderItemNums = electronicStoreItemService.buyItems(buyOrder);
-        return "요청하신 Item 중 " + orderItemNums + "개를 구매 하였습니다.";
-    }
-
-    @ApiOperation("여러 Item types 검색 (쿼리문)")
-    @GetMapping("/items-types")
-    public List<Item> findItemByTypes(
-            @RequestParam("type") List<String> types){
-        log.info("/items-types 요청 ids: " + types);
+    @ApiOperation("type 검색")
+    @GetMapping("/items-type")
+    public List<Item> findItemsByType(@RequestParam("type") List<String> types){
+        log.info("GET /items-type 요청이 들어왔습니다.");
         List<Item> items = electronicStoreItemService.findItemsByTypes(types);
-        log.info("/items-types  응답: " + items);
+        log.info("GET /items-type 응답: " + items);
         return items;
     }
-    @ApiOperation("단일 Item id로 검색 (쿼리문)")
+
+    @ApiOperation("가성비 싼 거부터 검색")
     @GetMapping("/items-prices")
-    public List<Item> findItemByPrices(
-            @RequestParam("max") Integer maxValue){
-        return electronicStoreItemService.findItemsOrderByPrices(maxValue);
+    public List<Item> findItemsByPricing(@RequestParam("max") Integer maxPrice){
+        log.info("GET /items-prices 요청이 들어왔습니다.");
+        List<Item> items = electronicStoreItemService.findItemsOrderByPrices(maxPrice);
+        log.info("GET /items-prices 응답: " + items);
+        return items;
     }
 
-    @ApiOperation("pagination 지원")
+    @ApiOperation("pagnation 지원")
     @GetMapping("/items-page")
     public Page<Item> findItemsPagination(Pageable pageable){
-        return electronicStoreItemService.findAllWithPageable(pageable);
+        log.info("GET /items-page 요청이 들어왔습니다.");
+        Page<Item> itemsPage = electronicStoreItemService.findAllWithPageable(pageable);
+        log.info("GET /items-page 응답: " + itemsPage);
+        return itemsPage;
     }
 
-    @ApiOperation("pagination 지원 2")
+    @ApiOperation("pagnation 지원")
     @GetMapping("/items-types-page")
-    public Page<Item> findItemsPagination(@RequestParam("type") List<String> types, Pageable pageable){
-        return electronicStoreItemService.findAllWithPageable(types, pageable);
+    public Page<Item> findItemsTypesPagination(@RequestParam("type") List<String> types ,Pageable pageable){
+        log.info("GET /items-types-page 요청이 들어왔습니다.");
+        Page<Item> itemsPage = electronicStoreItemService.findAllWithPageable(types, pageable);
+        log.info("GET /items-types-page 응답: " + itemsPage);
+        return itemsPage;
     }
 
     @ApiOperation("전체 stores 정보 검색")
     @GetMapping("/stores")
     public List<StoreInfo> findAllStoreInfo(){
-        return electronicStoreItemService.findAllStoreInfo();
+        log.info("GET /stores 요청이 들어왔습니다.");
+        List<StoreInfo> storeInfos = electronicStoreItemService.findAllStoreInfo();
+        log.info("GET /stores 응답: " + storeInfos);
+        return storeInfos;
     }
-
 }
+
+
