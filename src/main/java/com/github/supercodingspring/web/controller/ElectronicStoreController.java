c4e234fe2563d95240c395a4996087840085b7cd
VampireDeers <likemin5517@naver.com>
2023-08-06 20:29:48 +0900

    chore: ElectronicStoreController Log 주석 처리

--- src/main/java/com/github/supercodingspring/web/controller/ElectronicStoreController.java
+++ src/main/java/com/github/supercodingspring/web/controller/ElectronicStoreController.java
@@ -26,18 +26,16 @@ public class ElectronicStoreController {
     @ApiOperation("모든 Items을 검색")
     @GetMapping("/items")
     public List<Item> findAllItem(){
-        log.info("GET /items 요청이 들어왔습니다.");
         List<Item> items = electronicStoreItemService.findAllItem();
-        log.info("GET /items 응답: " + items);
         return items;
     }
 
     @ApiOperation("단일 Item 등록")
     @PostMapping("/items")
     public String registerItem(@RequestBody ItemBody itemBody){
-        log.info("POST /items 요청이 들어왔습니다.");
+//        log.info("POST /items 요청이 들어왔습니다.");
         Integer itemId = electronicStoreItemService.savaItem(itemBody);
-        log.info("POST /items 응답 ID: " + itemId);
+//        log.info("POST /items 응답 ID: " + itemId);
         return "ID: " + itemId;
     }
 
@@ -45,9 +43,7 @@ public class ElectronicStoreController {
     @GetMapping("/items/{id}")
     public Item findItemByPathId(
             @ApiParam(name = "id", value = "item ID", example = "1") @PathVariable String id){
-        log.info("GET /items/" + id + " 요청이 들어왔습니다.");
         Item item = electronicStoreItemService.findItemById(id);
-        log.info("GET /items/" + id + " 응답: " + item);
         return item;
     }
 
@@ -56,92 +52,86 @@ public class ElectronicStoreController {
     public Item findItemByQueryId(
             @ApiParam(name = "id", value = "item ID", example = "1")
             @RequestParam("id") String id){
-        log.info("GET /items-query 요청이 들어왔습니다.");
         Item item = electronicStoreItemService.findItemById(id);
-        log.info("GET /items-query 응답: " + item);
         return item;
     }
 
     @ApiOperation("여러 Item ids로 검색 (쿼리문)")
     @GetMapping("/items-queries")
     public List<Item> findItemByQueryIds(@ApiParam(name = "ids", value = "item IDs", example = "[1,2,3]") @RequestParam("id") List<String> ids){
-        log.info("/items-queries 요청 ids: " + ids);
         List<Item> items = electronicStoreItemService.findItemsByIds(ids);
-        log.info("/items-queries 응답: " + items);
         return items;
     }
 
     @ApiOperation("단일 Item id로 삭제")
     @DeleteMapping("/items/{id}")
     public String deleteItemByPathId(@ApiParam(name = "id", value = "item ID", example = "1") @PathVariable String id){
-        log.info("DELETE /items/" + id + " 요청이 들어왔습니다.");
         electronicStoreItemService.deleteItem(id);
         String responseMessage = "Object with id = " + id + " has been deleted";
-        log.info("DELETE /items/" + id + " 응답: " + responseMessage);
         return responseMessage;
     }
 
     @ApiOperation("단일 Item id 수정")
     @PutMapping("/items/{id}")
     public Item updateItem(@PathVariable String id, @RequestBody ItemBody itemBody){
-        log.info("PUT /items/" + id + " 요청이 들어왔습니다.");
+//        log.info("PUT /items/" + id + " 요청이 들어왔습니다.");
         Item updatedItem = electronicStoreItemService.updateItem(id, itemBody);
-        log.info("PUT /items/" + id + " 응답: " + updatedItem);
+//        log.info("PUT /items/" + id + " 응답: " + updatedItem);
         return updatedItem;
     }
 
     @ApiOperation("단일 Item 구매")
     @PostMapping("/items/buy")
     public String buyItem(@RequestBody BuyOrder buyOrder){
-        log.info("POST /items/buy 요청이 들어왔습니다.");
+//        log.info("POST /items/buy 요청이 들어왔습니다.");
         Integer orderItemNums = electronicStoreItemService.buyItems(buyOrder);
         String responseMessage = "요청하신 Item 중 " + orderItemNums + "개를 구매 하였습니다.";
-        log.info("POST /items/buy 응답: " + responseMessage);
+//        log.info("POST /items/buy 응답: " + responseMessage);
         return responseMessage;
     }
 
     @ApiOperation("type 검색")
     @GetMapping("/items-type")
     public List<Item> findItemsByType(@RequestParam("type") List<String> types){
-        log.info("GET /items-type 요청이 들어왔습니다.");
+//        log.info("GET /items-type 요청이 들어왔습니다.");
         List<Item> items = electronicStoreItemService.findItemsByTypes(types);
-        log.info("GET /items-type 응답: " + items);
+//        log.info("GET /items-type 응답: " + items);
         return items;
     }
 
     @ApiOperation("가성비 싼 거부터 검색")
     @GetMapping("/items-prices")
     public List<Item> findItemsByPricing(@RequestParam("max") Integer maxPrice){
-        log.info("GET /items-prices 요청이 들어왔습니다.");
+//        log.info("GET /items-prices 요청이 들어왔습니다.");
         List<Item> items = electronicStoreItemService.findItemsOrderByPrices(maxPrice);
-        log.info("GET /items-prices 응답: " + items);
+//        log.info("GET /items-prices 응답: " + items);
         return items;
     }
 
     @ApiOperation("pagnation 지원")
     @GetMapping("/items-page")
     public Page<Item> findItemsPagination(Pageable pageable){
-        log.info("GET /items-page 요청이 들어왔습니다.");
+//        log.info("GET /items-page 요청이 들어왔습니다.");
         Page<Item> items = electronicStoreItemService.findAllWithPageable(pageable);
-        log.info("GET /items-page 응답: " + items);
+//        log.info("GET /items-page 응답: " + items);
         return items;
     }
 
     @ApiOperation("pagnation 지원")
     @GetMapping("/items-types-page")
     public Page<Item> findItemsTypesPagination(@RequestParam("type") List<String> types, Pageable pageable){
-        log.info("GET /items-types-page 요청이 들어왔습니다.");
+//        log.info("GET /items-types-page 요청이 들어왔습니다.");
         Page<Item> items = electronicStoreItemService.findAllWithPageable(types, pageable);
-        log.info("GET /items-types-page 응답: " + items);
+//        log.info("GET /items-types-page 응답: " + items);
         return items;
     }
 
     @ApiOperation("전체 stores 정보 검색")
     @GetMapping("/stores")
     public List<StoreInfo> findAllStoreInfo(){
-        log.info("GET /stores 요청이 들어왔습니다.");
+//        log.info("GET /stores 요청이 들어왔습니다.");
         List<StoreInfo> storeInfos = electronicStoreItemService.findAllStoreInfo();
-        log.info("GET /stores 응답: " + storeInfos);
+//        log.info("GET /stores 응답: " + storeInfos);
         return storeInfos;
     }
 }
