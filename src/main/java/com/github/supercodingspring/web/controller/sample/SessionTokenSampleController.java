8779ad224f828bac0cd0c3009d7cfa64720f7815
VampireDeers <likemin5517@naver.com>
2023-08-07 01:04:19 +0900

    feat: 실습-2 Uwt 방식 세팅 및 구현

--- src/main/java/com/github/supercodingspring/web/controller/sample/SessionTokenSampleController.java
+++ src/main/java/com/github/supercodingspring/web/controller/sample/SessionTokenSampleController.java
@@ -1,10 +1,13 @@
 package com.github.supercodingspring.web.controller.sample;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @RestController
@@ -35,5 +38,39 @@ public class SessionTokenSampleController {
         return String.format("안녕하세요, 직업: %s 성별: %s인 %s 입니다.", job, gender, user);
     }
 
+    @GetMapping("/generate-token")
+    public String generateToken(HttpServletResponse httpServletResponse){
+        String jwt = Jwts.builder()
+                .setSubject("token1")
+                .claim("user", "조인성")
+                .claim("gender", "남자")
+                .claim("job", "배우")
+                .compact();
+        httpServletResponse.addHeader("Token", jwt);
+        return "JWT set Successfully";
+    }
 
+    @GetMapping("/generate-token2")
+    public String generateToken2(HttpServletResponse httpServletResponse){
+        String jwt = Jwts.builder()
+                         .setSubject("token1")
+                         .claim("user", "송혜교")
+                         .claim("gender", "여자")
+                         .claim("job", "배우")
+                         .compact();
+        httpServletResponse.addHeader("Token", jwt);
+        return "JWT set Successfully";
+    }
+    @GetMapping("/show-token")
+    public String showToken(@RequestHeader("Token") String token){
+        Claims claims = Jwts.parser()
+                .parseClaimsJwt(token)
+                .getBody();
+
+        String user = (String) claims.get("user");
+        String gender = (String) claims.get("gender");
+        String job = (String) claims.get("job");
+
+        return String.format("안녕하세요, 직업: %s 성별: %s인 %s 입니다.", job, gender, user);
+    }
 }
